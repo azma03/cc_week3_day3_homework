ad30af55c8f046f4e818e5fe240b52d3b75c1596
Asma <asma.virgo85@gmail.com>
2018-09-19 22:58:55 +0100

    Adds delete_all mthod

--- db/console.rb
+++ db/console.rb
@@ -1,43 +1,31 @@
 require("pry")
-require_relative("../models/artists")
-require_relative("../models/albums")
+require_relative("../models/artist")
+require_relative("../models/album")
 
-
-
+Album.delete_all()
+Artist.delete_all()
 
 artist1 = Artist.new({
-
   'name' => 'Prince'
-
-
   })
 
 artist1.save()
 
-
-
 artist2 = Artist.new({
-
   'name' => 'More Prince'
-
   })
 
 artist2.save()
 
-
 album1 = Album.new({
-
   'title' => 'Purple Rain',
   'genre' => 'All',
   'artist_id' => artist1.id
-
-
   })
 
-  album1.save()
+album1.save()
 
 album2 = Album.new({
-
   'title' => 'Diamonds and Pearls',
   'genre' => 'Ballad',
   'artist_id' => artist1.id
@@ -45,29 +33,26 @@ album2 = Album.new({
 
 album2.save()
 
-
-albums_artist = album2.list_artist()
-
 artists = Artist.all()
 albums = Album.all()
 
 collection = artist1.list_albums()
 
+albums_artist = album2.list_artist()
+
 artist1.name = 'Robert'
 artist1.update_artist()
-#
+
 album2.title = 'Cottage & Cheese'
 album2.update_album()
 
+album2.delete_album()
 
 artist2.delete_artist()
-
-album2.delete_album()
 
 found_artist = Artist.find_artist(artist1.id)
 
 found_album = Album.find_album(album1.id)
-
 
 binding.pry
 nil
--- models/album.rb
+++ models/album.rb
@@ -2,18 +2,15 @@ require_relative("../db/sql_runner")
 
 class Album
 
-attr_accessor :title, :genre
-attr_reader :id, :artist_id
+  attr_accessor :title, :genre
+  attr_reader :id, :artist_id
 
   def initialize(options)
-
     @id = options["id"].to_i if options["id"]
     @title = options["title"]
     @genre = options["genre"]
     @artist_id = options["artist_id"].to_i
-
   end
-
 
   def save()
     sql = "INSERT INTO albums (title, genre, artist_id) VALUES ($1, $2, $3) RETURNING id"
@@ -22,18 +19,11 @@ attr_reader :id, :artist_id
     @id = result[0]["id"].to_i
   end
 
-  def self.all()
-    sql = "SELECT * FROM albums"
-    result = SqlRunner.run(sql)
-    return result.map{|result_hash| Album.new(result_hash)}
-  end
-
   def list_artist()
     sql = "SELECT * FROM artists WHERE id = $1"
     values = [@artist_id]
     result = SqlRunner.run(sql, values)
     return result.map{|result_hash| Artist.new(result_hash)}
-
   end
 
   def update_album()
@@ -48,13 +38,22 @@ attr_reader :id, :artist_id
     SqlRunner.run(sql, values)
   end
 
+  def self.all()
+    sql = "SELECT * FROM albums"
+    result = SqlRunner.run(sql)
+    return result.map{|result_hash| Album.new(result_hash)}
+  end
+
   def self.find_album(id)
     sql = "SELECT * FROM albums WHERE id = $1"
     values = [id]
     result = SqlRunner.run(sql, values)
-    return result.map {|result_hash| Album.new(result_hash)}
-
+    return Album.new(result[0])
   end
 
+  def self.delete_all()
+    sql = "DELETE FROM albums"
+    SqlRunner.run(sql)
+  end
 
 end
